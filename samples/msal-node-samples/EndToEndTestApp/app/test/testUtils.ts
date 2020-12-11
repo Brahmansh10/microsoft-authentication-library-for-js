7e9ca5e4e0b5a026b3730045af19dbe8462d21c0
Samuel Kubai <samuel.kamau@microsoft.com>
2020-12-11 17:54:42 +0300

    feat(msal-node): extend by adding the device code base flow
    - add the device code base flow e2e test

--- samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
+++ samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
@@ -6,6 +6,7 @@ export async function enterCredentials(page: Page, screenshot: Screenshot, usern
     await screenshot.takeScreenshot(page, `loginPage`);
     await page.type("#i0116", username);
     await page.click("#idSIButton9");
+    await new Promise(resolve => setTimeout(() => screenshot.takeScreenshot(page, 'afterEmailSubmission').then(() => resolve()), 5000));
     await page.waitForSelector("#idA_PWD_ForgotPassword");
     await screenshot.takeScreenshot(page, `pwdInputPage`);
     await page.type("#i0118", accountPwd);
@@ -27,4 +28,21 @@ export async function enterCredentialsADFS(page: Page, screenshot: Screenshot, u
     await screenshot.takeScreenshot(page, `adfsUsernameInputPage`);
     await page.type("#passwordInput", accountPwd);
     await page.click("#submitButton");
+}
+
+export async function enterDeviceCode(page: Page, screenshot: Screenshot, code: string, deviceCodeUrl: string): Promise<void> {
+    await page.goto(deviceCodeUrl);
+    await page.waitForSelector("#otc");
+    await screenshot.takeScreenshot(page, 'deviceCodePage');
+    await page.type("#otc", code);
+    await page.click("#idSIButton9");
+}
+
+export function extractDeviceCode(output: string): string | null {
+    if ((/code (?<code>\w+)/).test(output)) {
+        const matches = output.match(/code (?<code>\w+)/);
+        return matches.groups.code;
+    }
+
+    return null;
 }
